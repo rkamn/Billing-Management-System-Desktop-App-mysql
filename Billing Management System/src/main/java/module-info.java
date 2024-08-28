f8099b46b80a9d31cdb420a621be792f6d02762a
rkamn <rakeshkumar458458@gmail.com>
2024-08-28 09:56:32 +0530

    creating deployable jar

--- Billing Management System/src/main/java/module-info.java
+++ Billing Management System/src/main/java/module-info.java
@@ -8,8 +8,8 @@ module in.lightbits.billingmanagementsystem {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
-    requires pdfbox.app;
-    //requires org.apache.pdfbox;
+    //requires pdfbox.app;
+    requires org.apache.pdfbox;
     // requires eu.hansolo.tilesfx;
 
     opens in.lightbits.billingmanagementsystem to javafx.fxml;
